a35fa3fd69393873a963fabc8d002a898ca2e1f3
karen <kkwentus@chef.io>
2018-02-25 09:39:35 -0600

    updated

---
+++ dca/6_stagAndProd.ps1
@@ -0,0 +1,6 @@
+Update-RunLists staging audit
+Update-RunLists staging hardening
+Invoke-ChefClient staging
+Update-RunLists production audit
+Update-RunLists production hardening
+Invoke-ChefClient production
