3d44c76464f2d691195c209b401d79f74b1a3505
karen <kkwentus@chef.io>
2018-02-25 09:25:14 -0600

    updated

---
+++ dca/4_staging.ps1
@@ -0,0 +1,5 @@
+DCA-Update-Nodes staging audit
+Update-RunLists staging hardening
+Invoke-ChefClient staging
+Update-RunLists staging install_site
+Invoke-ChefClient staging
---
+++ dca/5_prod.ps1
@@ -0,0 +1,3 @@
+DCA-Update-Nodes prod audit
+Update-RunLists production hardening
+Invoke-ChefClient production
