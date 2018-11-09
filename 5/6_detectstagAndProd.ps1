cf1092a89c3b278792ab8d7baa73db1bdbb2c545
karen <kkwentus@chef.io>
2018-11-09 14:44:52 -0600

    67

---
+++ 5/6_detectstagAndProd.ps1
@@ -0,0 +1,4 @@
+Update-RunLists staging dca_demo audit
+Invoke-ChefClient staging
+Update-RunLists production dca_demo audit
+Invoke-ChefClient production
--- 5/7_correctStageAndProd.ps1
+++ 5/7_correctStageAndProd.ps1
@@ -1,6 +1,4 @@
-Update-RunLists staging dca_demo audit
 Update-RunLists staging dca_demo hardening
 Invoke-ChefClient staging
-Update-RunLists production dca_demo audit
 Update-RunLists production dca_demo hardening
 Invoke-ChefClient production
